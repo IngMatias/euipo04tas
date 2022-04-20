4ff8d9bfe69ed20360262ffdd8bd7a7d1897e35f
Matias <matias.hernandez@correo.ucu.edu.uy>
2022-04-20 19:50:22 -0300

    Corchetes Implementados

---
+++ UT3_TA6/src/main/java/ta1/mavenproject2/Pila.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ta1.mavenproject2;
+
+/**
+ *
+ * @author ingFo
+ */
+public class Pila<T> implements IPila<T>{
+    
+    private Lista<T> lista = new Lista();
+    
+    public boolean apilar(T dato) {
+        
+    }
+    
+    public T desapilar() {
+        
+    }
+    
+    public T tope() {
+        
+    }
+    
+    public boolean esVacia() {
+        
+    }
+    
+    public void vaciar() {
+        
+    }
+    
+    public boolean estaLlena() {
+        
+    }
+}
